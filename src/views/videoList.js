0e59f4e379e7bf56e45985cc8b0912e5f0735afc
jehwa <s.jehwa@gmail.com>
2018-05-07 21:10:51 -0700

    Pass tests for VideoListView, VideoListEntryView, and VideoPlayerView

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,5 +1,17 @@
 var VideoListView = Backbone.View.extend({
+  
+  initialize: function() {
 
+    this.render();
+    console.log(this.$el);
+    
+    this.collection.models.forEach(function(video) {
+      var view = new VideoListEntryView({'model': video});
+      this.$el.append(view.$el);
+    }.bind(this));
+
+      
+  },
 
   render: function() {
     this.$el.children().detach();
--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,6 +1,14 @@
 var VideoListEntryView = Backbone.View.extend({
 
+  initialize: function() {
 
+    this.render();
+    this.$el.on('click', function() {
+      this.model.select();
+    }.bind(this));
+  },
+  
+  
   render: function() {
     this.$el.html(this.template(this.model.attributes));
     return this;
